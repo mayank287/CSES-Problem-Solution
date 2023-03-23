21001590ede4dbe87e40d88b6cf5fe0d5ca002fa
Mayank Arora <83607303+mayank287@users.noreply.github.com>
2023-03-23 11:50:21 +0530

    Create Editdistance.cpp

---
+++ Editdistance.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+const int mod = 1000000007;
+int main()
+{
+    string word1,word2;
+    cin >> word1 >> word2;
+    
+ 
+        int m = word1.length();
+        int n = word2.length();
+        int dp[m + 1][n + 1];
+ 
+        for(int i = 0; i <= m; i++)
+        {
+            dp[i][0] = i;
+        }
+ 
+        for(int j = 0; j <= n; j++)
+        {
+            dp[0][j] = j;
+        }
+ 
+        for(int i = 1; i <= m; i++)
+        {
+            for(int j = 1; j<=n; j++)
+            {
+                if(word1[i - 1] == word2[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else{
+                    dp[i][j] =1 + min(dp[i - 1][j],min(dp[i][j - 1]
+                    ,dp[i - 1][j - 1]));
+                }
+            }
+        }
+ 
+      cout <<  dp[m][n] <<  endl;
+     
+       
+ 
+    return 0;
+}
